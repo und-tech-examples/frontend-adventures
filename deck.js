8c0c47dda694204c6755cd249871f86da6500799
Claudia Valdivieso <claumavaldivieso@gmail.com>
2019-01-10 14:44:14 -0500

    Add resources slide

--- deck.js
+++ deck.js
@@ -1,9 +1,11 @@
 import intro from './intro.mdx'
 import daniela from './daniela-deck.mdx'
 import kathia from './kathia-deck.mdx'
+import resources from './resources.mdx'
 
 export default [
   ...intro,
   ...daniela,
-  ...kathia
+  ...kathia,
+  ...resources
 ]
